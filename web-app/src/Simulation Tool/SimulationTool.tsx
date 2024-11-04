bbfe65005af2d2a2efa1a698c048dfbeae1e27a0
Shain Sahagun <qsaqsahagun@tip.edu.ph>
2024-11-04 22:38:12 +0800

    Added a side panel on the simulation page.

--- web-app/src/Simulation Tool/SimulationTool.tsx
+++ web-app/src/Simulation Tool/SimulationTool.tsx
@@ -1,11 +1,17 @@
 // src/Simulation Tool/SimulationTool.tsx
 import React from 'react';
+import './SimulationTool.css'; // Make sure to import the CSS file
 
 function SimulationTool() {
   return (
-    <div>
-      <h1>Simulation Shit</h1>
-      {/* Add your content here */}
+    <div className="simulation-container">
+      {/* Side Panel */}
+      <div className="side-panel" />
+
+      {/* Main Content Area */}
+      <div className="main-content">
+        {/* Add your main content here */}
+      </div>
     </div>
   );
 }
