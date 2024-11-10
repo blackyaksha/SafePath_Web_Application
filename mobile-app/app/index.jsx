8609cdd7670abd5427f6313717704ef6eae796db
Jhoana <qjebalawang@tip.edu.ph>
2024-11-10 16:41:02 +0800

    Fonts & Color Paletttes Loaded

--- mobile-app/app/index.jsx
+++ mobile-app/app/index.jsx
@@ -1,11 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View } from 'react-native';
 import { Link } from 'expo-router';
 
 export default function App() {
     return (
         <View className="flex-1 items-center justify-center bg-white">
-            <Text className="text-3xl">SafePath</Text>
+            <Text className="text-3xl font-black">SafePath</Text>
             <StatusBar style="auto"/>
             <Link href="/profile" style={{ color: 'blue' }}>Go to Profile</Link>  
         </View>
--- mobile-app/tailwind.config.js
+++ mobile-app/tailwind.config.js
@@ -4,7 +4,37 @@ module.exports = {
   content: ["./app/**/*.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}"],
   presets: [require("nativewind/preset")],
   theme: {
-    extend: {},
+    extend: {
+      extend: {
+        colors: {
+          primary: "#FFFFFF",
+          secondary: {
+            DEFAULT: "#C4C98C",
+            100: "#AAD400",
+            200: "#FF8E01",
+          },
+          black: {
+            DEFAULT: "#000000",
+            100: "#1E1E2D",
+            200: "#232533",
+          },
+          gray: {
+            100: "#CECECE",
+          },
+        },
+        fontFamily: {
+          ithin: ["Inter-Thin"],
+          iextralight: ["Inter-ExtraLight"],
+          ilight: ["Inter-Light"],
+          iregular: ["Inter-Regular"],
+          imedium: ["Inter-Medium"],
+          isemibold: ["Inter-SemiBold"],
+          ibold: ["Inter-Bold"],
+          iextrabold: ["Inter-ExtraBold"],
+          iblack: ["Inter-Black"],
+        },
+      },
+    },
   },
   plugins: [],
 }
